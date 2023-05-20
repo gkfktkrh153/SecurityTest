82def7d35e80e1dc2652e70e694a15b9c5d6884e
gkfktkrh153 <gkfktkrh153@naver.com>
2023-05-20 14:45:53 +0900

    HttpServletSession을 이용한 로그인 구현

--- src/main/java/com/example/securitytest/controller/MemberController.java
+++ src/main/java/com/example/securitytest/controller/MemberController.java
@@ -20,36 +20,38 @@ import java.util.Optional;
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-    private final SessionManager sessionManager;
     private final MemberService memberService;
     @GetMapping("/")
     public String home(HttpServletRequest request, Model model) {
-        Member member = sessionManager.getSession(request);
+        HttpSession session = request.getSession(false);
         // 세션이 존재한다면 세션에 저장되있는 member조회
 
-        if(member == null) { // 아직 인증 X
+        if(session == null) { // 아직 인증 X
             return "login";
         }
-
-        model.addAttribute("member", member);
+        Member loginMember = (Member) session.getAttribute("LOGIN_MEMBER");
+        model.addAttribute("member", loginMember);
         return "home";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute MemberDto memberDto, HttpServletResponse response){
+    public String login(@ModelAttribute MemberDto memberDto, HttpServletRequest request){
         Member loginMember = memberService.login(memberDto);
 
         if(loginMember == null) // 로그인 실패
             return "redirect:/"; // 세션없이 홈으로 이동 -> 로그인 페이지 이동
 
-        sessionManager.createSession(loginMember, response); // 세션 생성
+        HttpSession session = request.getSession();
+        session.setAttribute("LOGIN_MEMBER", loginMember);
 
         return "redirect:/"; // 세션이 존재하므로 home으로 이동
     }
 
     @PostMapping("logout")
     public String logout(HttpServletRequest request) {
-        sessionManager.sessionExpire(request);
+        HttpSession session = request.getSession(false);
+        if(session != null) // 세션이 존재한다면
+            session.invalidate(); // 세션 만료
         return "redirect:/";
     }
 }
