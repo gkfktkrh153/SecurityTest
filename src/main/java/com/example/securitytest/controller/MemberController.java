2ed8e35e6ab4cae136ac5cea89d8e4e66b176817
gkfktkrh153 <gkfktkrh153@naver.com>
2023-05-22 23:52:51 +0900

    Spring security 로그인 구현

--- src/main/java/com/example/securitytest/controller/MemberController.java
+++ src/main/java/com/example/securitytest/controller/MemberController.java
@@ -1,12 +1,8 @@
 package com.example.securitytest.controller;
 
-import com.example.securitytest.Session;
-import com.example.securitytest.SessionManager;
 import com.example.securitytest.entity.Member;
 import com.example.securitytest.entity.dto.MemberDto;
-import com.example.securitytest.repository.MemberRepository;
 import com.example.securitytest.service.MemberService;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -15,17 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpRequest;
-import java.util.Optional;
-
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-    private final SessionManager sessionManager;
     private final MemberService memberService;
+
     @GetMapping("/")
     public String home(HttpServletRequest request, Model model) {
-        Session session = sessionManager.getSession(request);
+        HttpSession session = request.getSession();
 
         if(session == null) { // 아직 인증 X
             return "login";
@@ -35,15 +28,23 @@ public class MemberController {
         model.addAttribute("member", loginMember);
         return "home";
     }
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+
+        if(session == null) { // 아직 인증 X
+            return "login";
+        }
+        return "home";
+    }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute MemberDto memberDto, HttpServletResponse response){
+    public String login(@ModelAttribute MemberDto memberDto, HttpServletResponse response, HttpServletRequest request){
         Member loginMember = memberService.login(memberDto);
 
         if(loginMember == null) // 로그인 실패
             return "redirect:/"; // 세션없이 홈으로 이동 -> 로그인 페이지 이동
-
-        Session session = sessionManager.createSession(loginMember, response);// 세션 생성
+        HttpSession session = request.getSession();
         session.setAttribute("LOGIN_MEMBER", loginMember);
 
         return "redirect:/"; // 세션이 존재하므로 home으로 이동
@@ -51,7 +52,8 @@ public class MemberController {
 
     @PostMapping("logout")
     public String logout(HttpServletRequest request) {
-        sessionManager.sessionExpire(request);
+        HttpSession session = request.getSession();
+        session.invalidate();
         return "redirect:/";
     }
 }
